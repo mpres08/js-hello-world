3f8c61e884aa098f4974283d71e2d08ba21cb8d5
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-05-20 12:15:51 -0500

    no semi-colon

--- hello.js
+++ hello.js
@@ -4,8 +4,7 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log(Hello, World!);
-console.log("code must have set of double quotes to work");
+console.log(Hello, World!)
 
 /*  
 Next, PLAY! 
